11540f02ff7f7e9654ceb784408be8dbb9ce35fe
SchunckCCS <125136575+SchunckCCS@users.noreply.github.com>
2023-09-12 22:29:13 -0300

    Exercicio 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -6,3 +6,12 @@ FROM livros;
 SELECT nome
 FROM autores
 WHERE ano_nascimento < 1900;
+
+--03
+SELECT titulo
+FROM livros
+WHERE id_autor = (
+  SELECT id
+  FROM autores
+  WHERE nome = 'J.K. Rowling'
+);
