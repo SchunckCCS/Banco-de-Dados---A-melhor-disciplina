766c28f6ea6e383b4a038c8ca28ff859ebbcfcbf
SchunckCCS <125136575+SchunckCCS@users.noreply.github.com>
2023-09-26 21:18:28 -0300

    Livros por Categoria

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,6 +1,16 @@
-
+--01
 CREATE PROCEDURE sp_ListarAutores
 AS
 BEGIN
     SELECT Nome, Sobrenome FROM Autores;
+END;
+
+--02
+CREATE PROCEDURE sp_LivrosPorCategoria
+    @Categoria NVARCHAR(50)
+AS
+BEGIN
+    SELECT Titulo
+    FROM Livros
+    WHERE Categoria = @Categoria;
 END;
